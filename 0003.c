4e249b4766be6dbbd5cf71c7aaae5ca76ae561a8
Dank0m <142665546+Dank0m@users.noreply.github.com>
2023-08-20 16:50:20 +0300

    1.9. Symbolic arrays

---
+++ 0003.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+//#define MAXLINE 1000 /* максимальный размер вводимой строки */
+
+int mygetline(char line[], int MAXLINE);
+void copy(char to[], char from[]);
+
+/* печать самой длинной строки */
+main()
+{
+    int len; /* длина текущей строки */
+    int max; /* длина максимальной из просмотренных строк */
+    char line[1000]; /* текущая строка */
+    char longest[1000]; /* самая длинная строка */
+
+    max = 0;
+    while ((len = mygetline(line, 1000)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+    if (max > 0) /* была ли хоть одна строка? */
+    printf("%s", longest);
+    return 0;
+}
+
+
+/* getline: читает строку в s, возвращает длину */
+int mygetline(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+        }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: копирует из 'from' в 'to'; to достаточно большой */
+void copy(char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
